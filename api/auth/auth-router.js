ff7a78d8f7e00d0726c3833316fea8813d433ac0
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-24 08:26:30 -0600

    mvp complete

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,11 @@
-const router = require("express").Router();
-const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../users/users-model')
+const { checkUsernameExists, validateRoleName } = require('./auth-middleware')
+const { JWT_SECRET, ROUNDS } = require('../secrets') // use this secret!
 
-router.post("/register", validateRoleName, (req, res, next) => {
+router.post('/register', validateRoleName, async (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -14,10 +17,22 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
-});
+  const credentials = req.body
+  try {
+    credentials.password = bcrypt.hashSync(credentials.password, ROUNDS)
+    credentials.role_name = req.role_name
+    const newUser = await User.add(credentials)
+    res.status(201).json({
+      user_id: newUser.user_id,
+      username: newUser.username,
+      role_name: newUser.role_name
+    })
+  } catch (err) {
+    next(err)
+  }
+})
 
-
-router.post("/login", checkUsernameExists, (req, res, next) => {
+router.post('/login', checkUsernameExists, (req, res, next) => {
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -37,6 +52,31 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
-});
+  try {
+    if (bcrypt.compareSync(req.body.password, req.userExists.password)) {
+      const token = buildToken(req.userExists)
+      res.json({
+        message: `${req.userExists.username} is back!`,
+        token: token
+      })
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' })
+    }
+  } catch (err) {
+    next(err)
+  }
+})
 
-module.exports = router;
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name
+  }
+  const config = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, JWT_SECRET, config)
+}
+
+module.exports = router
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,4 +1,4 @@
-const db = require('../../data/db-config.js');
+const db = require('../../data/db-config.js')
 
 function find() {
   /**
@@ -18,6 +18,9 @@ function find() {
       }
     ]
    */
+  return db('users as u')
+    .join('roles as r', 'u.role_id', 'r.role_id')
+    .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
@@ -34,6 +37,10 @@ function findBy(filter) {
       }
     ]
    */
+  return db('users as u')
+    .join('roles as r', 'u.role_id', 'r.role_id')
+    .where(filter)
+    .select('user_id', 'username', 'password', 'role_name')
 }
 
 function findById(user_id) {
@@ -47,6 +54,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+  return db('users as u')
+    .join('roles as r', 'u.role_id', 'r.role_id')
+    .where('user_id', user_id)
+    .first()
 }
 
 /**
@@ -67,9 +78,10 @@ function findById(user_id) {
     "role_name": "team lead"
   }
  */
-async function add({ username, password, role_name }) { // done for you
+async function add({ username, password, role_name }) {
+  // done for you
   let created_user_id
-  await db.transaction(async trx => {
+  await db.transaction(async (trx) => {
     let role_id_to_use
     const [role] = await trx('roles').where('role_name', role_name)
     if (role) {
@@ -78,7 +90,11 @@ async function add({ username, password, role_name }) { // done for you
       const [role_id] = await trx('roles').insert({ role_name: role_name })
       role_id_to_use = role_id
     }
-    const [user_id] = await trx('users').insert({ username, password, role_id: role_id_to_use })
+    const [user_id] = await trx('users').insert({
+      username,
+      password,
+      role_id: role_id_to_use
+    })
     created_user_id = user_id
   })
   return findById(created_user_id)
@@ -88,5 +104,5 @@ module.exports = {
   add,
   find,
   findBy,
-  findById,
-};
+  findById
+}
